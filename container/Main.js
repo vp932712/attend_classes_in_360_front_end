4bda59592fbd7e8bb48934bcf8ff2fca9cd0d608
Vaibhav Patel <vp932712@gmail.com>
2018-04-01 19:06:34 -0400

    0.0.3

---
+++ container/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  VrButton
+} from 'react-vr';
+
+class Main extends React.Component {
+
+  render() {
+    return (
+     
+    )
+  }
+}
+export default Main;
--- index.vr.js
+++ index.vr.js
@@ -16,21 +16,8 @@ export default class attend_classes_in_360_front_end extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
+        
+
       </View>
     );
   }
