5a6fcf0fcbd0f11a8c1149c0d2a3acbb9309866b
Vaibhav Patel <vp932712@gmail.com>
2018-04-01 17:36:22 -0400

    0.0.2

--- index.vr.js
+++ index.vr.js
@@ -3,9 +3,14 @@ import {
   AppRegistry,
   asset,
   Pano,
+  VideoPano,
   Text,
   View,
 } from 'react-vr';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 export default class attend_classes_in_360_front_end extends React.Component {
   render() {
